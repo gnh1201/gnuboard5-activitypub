c7ef2a455868a817e1bfcb4eeb008d227b873dfe
Namhyeon, Go <gnh1201@gmail.com>
2022-06-30 16:00:43 +0900

    Create activitypub-servers.php

---
+++ data/activitypub-servers.php
@@ -0,0 +1,38 @@
+<?php
+if (!defined('_GNUBOARD_')) exit;
+
+return <<<EOF
+{
+    "self": {
+		"enabled: true,
+        "platform": "gnuboard5",
+        "user": "/?route=activitypub.user&mb_id=:username",
+        "inbox": "/?route=activitypub.inbox",
+        "userinbox": "/?route=activitypub.inbox&mb_id=:username",
+        "docs": [
+            "https://github.com/gnh1201/gnuboard5-activitypub"
+        ]
+    },
+    "https://mastodon.social": {
+		"enabled": ture,
+        "platform": "mastodon",
+        "user": "/users/:username",
+        "inbox": "/inbox",
+        "userinbox": "/user/inbox/:username/inbox",
+        "accesstoken": "YOUR ACCESSTOKEN HERE",
+        "docs": [
+            "https://docs.joinmastodon.org/spec/activitypub/"
+        ]
+    },
+    "https://peertube.local": {
+		"enabled": false,
+        "platform": "peertube",
+        "user": "/accounts/:username",
+        "inbox": "/inbox",
+        "userinbox": "/accounts/:username/inbox",
+        "docs": [
+            "https://docs.joinpeertube.org/api-rest-reference.html"
+        ]
+    }
+}
+EOF;
