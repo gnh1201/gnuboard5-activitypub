4b0a364835b654129fdf825445fc213c107fa913
stackgo <gnh1201@gmail.com>
2022-07-04 17:01:10 +0900

    Update activitypub_form.php

--- adm/activitypub_form.php
+++ adm/activitypub_form.php
@@ -13,9 +13,9 @@ add_javascript(G5_POSTCODE_JS, 0);    //다음 주소 js
 
 <form name="factivity" id="factivity" action="./activitypub_form_update.php" method="post" enctype="multipart/form-data">
     <div>
-		<input type="hidden" name="token" value="">
-	</div>
-	
+        <input type="hidden" name="token" value="">
+    </div>
+    
     <div class="tbl_frm01 tbl_wrap">
         <table>
             <caption><?php echo $g5['title']; ?></caption>
@@ -30,22 +30,22 @@ add_javascript(G5_POSTCODE_JS, 0);    //다음 주소 js
                         <input type="text" id="object_id" name="object_id" value="" class="frm_input" size="45">
                     </td>
                 </tr>
-				<tr>
+                <tr>
                     <th scope="row"><label for="to">전송할 위치</label></th>
                     <td>
                         <input type="text" id="to" name="to" value="" class="frm_input" size="45"> (쉼표로 구분)
                     </td>
                 </tr>
-				<tr>
+                <tr>
                     <th scope="row"><label for="content">내용</label></th>
                     <td>
-						<textarea id="content" name="content" rows="20"></textarea>
+                        <textarea id="content" name="content" rows="20"></textarea>
                     </td>
-				</tr>
-			</tbody>
-		</table>
-	</div>
-	
+                </tr>
+            </tbody>
+        </table>
+    </div>
+    
     <div class="btn_fixed_top">
         <input type="submit" name="act_button" value="전송" onclick="document.pressed=this.value" class="btn btn_01">
     </div>
